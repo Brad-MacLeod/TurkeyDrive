dd1bfcf88a4030df239b9629f1e0bc0687887e9f
Bradley MacLeod <bradley.b.macleod@gmail.com>
2024-11-21 21:47:12 -0400

    Updated Progress

--- turkey-drive/src/app/app.component.ts
+++ turkey-drive/src/app/app.component.ts
@@ -4,14 +4,17 @@ import { FaqComponent } from "./tools/faq/faq.component";
 import { ProgressComponent } from "./tools/progress/progress.component";
 import { ScheduleComponent } from "./tools/schedule/schedule.component";
 import { LocationsComponent } from './tools/locations/locations.component';
+import { FireComponent } from "./tools/fire/fire.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FaqComponent, ProgressComponent, ScheduleComponent,LocationsComponent],
+  imports: [RouterOutlet, FaqComponent, ProgressComponent, ScheduleComponent, LocationsComponent, FireComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'turkey-drive';
+  goal:number = 150;
+  progress:number = 11;
 }
--- turkey-drive/src/app/tools/progress/progress.component.ts
+++ turkey-drive/src/app/tools/progress/progress.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 @Component({
@@ -9,5 +9,11 @@ import {MatProgressBarModule} from '@angular/material/progress-bar';
   styleUrl: './progress.component.scss'
 })
 export class ProgressComponent {
+  @Input() turkeys: number = 0;
+  @Input() goal: number = 150;
+
+  getProgress(){
+    return this.turkeys/this.goal*100;
+  }
 
 }
